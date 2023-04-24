42fe0c04f85879f9e9e94aa1396d31ecd489e1c0
Rubanov <Rubanov.work@gmail.com>
2023-04-24 23:16:38 +0300

    Задачи 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,74 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
+//        task9();
+//        task10();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] one = {1, 2, 3};
+        double[] two = {1.57, 7.654, 9.986};
+        int[] three = new int[13];
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] one = {1, 2, 3};
+        double[] two = {1.57, 7.654, 9.986};
+        int[] three = {7, 15, 4, 13, 18, 6, 9};
+        System.out.println(Arrays.toString(one));
+        System.out.println(Arrays.toString(two));
+        System.out.println(Arrays.toString(three));
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] one = {1, 2, 3};
+        double[] two = {1.57, 7.654, 9.986};
+        int[] three = {7, 15, 4, 13, 18, 6, 9};
+
+        for (int i = one.length - 1; i >= 0; i--) {
+            if (i == one.length - 3)
+                System.out.print(one[i]);
+            else
+                System.out.print(one[i] + ", ");
         }
+        System.out.println();
+
+        for (int i = two.length - 1; i >= 0; i--) {
+            if (i == two.length - 3)
+                System.out.print(two[i]);
+            else
+                System.out.print(two[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = three.length - 1; i >= 0; i--) {
+            if (i == three.length - 7)
+                System.out.print(three[i]);
+            else
+                System.out.print(three[i] + ", ");
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int[] one = {7, 5, 8};
+        for (int i = 0; i < one.length; i++) {
+            if (one[i] % 2 != 0) {
+                one[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(one));
     }
 }
